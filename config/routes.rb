6adac38a95ae6e6460e911746f08eab69a92f01c
李沛秦 <lipeiqin@lipeiqindeMacBook-Pro.local>
2016-04-17 17:25:03 -0500

    finish movie review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
   devise_for :users
 
   resources :movies do
+    collection do
+      get 'search'
+    end
+    
     resources :reviews, except: [:show, :index]
   end
 
